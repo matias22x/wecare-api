8218513af4db3da91349bfe5f290404837ff4c49
matias <matias@grava.digital>
2018-05-06 17:45:09 -0300

    agregadas variables a .env, modificaciones en app.js sobre primer admin y package actualizado

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ dotenv.load();
 
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const routes = require('./lib/routes');
 const extractJwt = require('./lib/routes/extract-jwt');
 const publicPath = require('./config/public');
+const user = require('./lib/models/user');
+const admin = require('./lib/models/admin');
+
 var app = express();
 
 
@@ -24,6 +27,7 @@ app.use(expressWinston.logger({
     statusLevels: true
 }));
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
@@ -46,6 +50,35 @@ app.use(publicPath.pathRegex, extractJwt);
 const buildBaucis = require('./build-baucis');
 const baucisInstance = buildBaucis();
 app.use('/api', baucisInstance);
+
+// admin
+user.findOne({username:process.env.ORGANIZATION_ADMIN_USERNAME, tipo:'admin'}, function(err, usuario) {
+    if (!usuario) {
+        var firstUser = new user({
+            username: process.env.ORGANIZATION_ADMIN_USERNAME,
+            password: process.env.ORGANIZATION_ADMIN_PASSWORD,
+            email: process.env.ORGANIZATION_ADMIN_EMAIL,
+            tipo: process.env.ORGANIZATION_ADMIN_TIPO
+        });
+        firstUser.save().then(function(response) {
+            var firstAdmin = new admin({
+              direccion: process.env.ORGANIZATION_ADMIN_DIRECCION,
+              nombre: process.env.ORGANIZATION_ADMIN_NOMBRE,
+              email: process.env.ORGANIZATION_ADMIN_EMAIL,
+              user: response
+            });
+
+            return firstAdmin.save();
+        }).then(function() {
+          logger.info('First admin created.');
+        }).catch(function(firstAdminErr) {
+            logger.error('firstAdminErr: ', firstAdminErr);
+        });
+    }else{
+        logger.info('Ya existe un admin para esta organizacion');
+    }
+
+});
 
 Object.keys(routes).forEach((key) => {
     app.use(routes[key]);
